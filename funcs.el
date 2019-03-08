8eb1b1fb137cadb3372d67f4b2697b5e72ae3636
Jesse Bridge <jesse.bridge@my.jcu.edu.au>
2019-03-08 16:56:29 +1000

    moved init config variables to func

--- funcs.el
+++ funcs.el
@@ -1,1 +1,7 @@
-(when (configuration-layer/package-usedp 'jesi))
+;;(when (configuration-layer/package-usedp 'jesi))
+
+(defun spacemacs/jesi-init ()
+        (spacemacs/toggle-indent-guide-globally-on)
+        (setq-default
+		dotspacemacs-line-numbers 'relative
+        	require-final-newline t))
--- packages.el
+++ packages.el
@@ -1,8 +1,8 @@
 (defconst jesi-packages
 	 '(jesi))
 
-(defun jesi/init-jesi ()
-	(spacemacs/toggle-indent-guide-globally-on)
-	(setq dotspacemacs-line-numbers 'relative)
-        (setq require-final-newline t)
-)
+;;(defun jesi/init-jesi ()
+;;	(spacemacs/toggle-indent-guide-globally-on)
+;;	(setq dotspacemacs-line-numbers 'relative)
+;;        (setq require-final-newline t)
+;;)
