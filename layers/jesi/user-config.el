6be2685312bc7de16520e097ecaada0151adf084
André de Jager <andrethehunter@gmail.com>
2019-03-12 16:48:00 +1000

    WIP Java eclim

--- layers/jesi/user-config.el
+++ layers/jesi/user-config.el
@@ -21,7 +21,8 @@
 
 
 (setq 
-  eclim-eclipse-dirs '("/Applications/Eclipse\ Java.app/Contents/Eclipse/")
-  eclimd-executable "/Applications/Eclipse\\ Java.app/Contents/Eclipse/plugins/org.eclim_2.8.0/bin/eclimd"
-  eclim-executable "/Applications/Eclipse\\ Java.app/Contents/Eclipse/plugins/org.eclim_2.8.0/bin/eclim"
+  eclim-eclipse-dirs '("/Applications/Eclipse Java.app/Contents/Eclipse")
+  eclimd-executable "/Applications/Eclipse Java.app/Contents/Eclipse/eclimd"
+  eclim-executable "/Applications/Eclipse Java.app/Contents/Eclipse/plugins/org.eclim_2.8.0/bin/eclim"
+  eclimd-default-workspace "~/eclipse-workspace"
   eclimd-wait-for-process t)
