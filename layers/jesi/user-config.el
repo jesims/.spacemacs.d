b95822e84c183f438da71d9101efbb2045c5c902
Alexander Scott <xander@cadcoder.com>
2019-03-12 16:53:19 +1000

    Remove lispy

--- layers/jesi/user-config.el
+++ layers/jesi/user-config.el
@@ -9,7 +9,6 @@
           '(defaults       ; should be included.
             pretty-parens  ; different paren styles for different modes.
             evil           ; If you use Evil.
-            lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
             paredit        ; Introduce some paredit commands.
             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
             smart-yank))   ; Yank behavior depend on mode.
