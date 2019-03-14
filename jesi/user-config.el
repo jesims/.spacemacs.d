737d9a4c02346eeff8818051006db408fc776a7d
Alexander Scott <xander@cadcoder.com>
2019-03-14 19:38:30 +1000

    Fix parinfer

--- jesi/user-config.el
+++ jesi/user-config.el
@@ -6,12 +6,12 @@
   :init
   (progn
     (setq parinfer-extensions
-          '(defaults       ; should be included.
-            pretty-parens  ; different paren styles for different modes.
-            evil           ; If you use Evil.
-            paredit        ; Introduce some paredit commands.
-            smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-            smart-yank))   ; Yank behavior depend on mode.
+     '(defaults       ; should be included.
+       pretty-parens  ; different paren styles for different modes.
+       evil           ; If you use Evil.
+       paredit        ; Introduce some paredit commands.
+       smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+       smart-yank))   ; Yank behavior depend on mode.
     (add-hook 'clojure-mode-hook #'parinfer-mode)
     (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
