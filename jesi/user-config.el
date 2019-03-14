53086cbd73faf305dfc1b6ab925ee930802a50c2
Alexander Scott <xander@cadcoder.com>
2019-03-14 19:43:08 +1000

    Less config for parinfer

--- jesi/user-config.el
+++ jesi/user-config.el
@@ -1,23 +1,21 @@
 (spacemacs/toggle-indent-guide-globally-on)
-(use-package parinfer
-  :ensure t
-  :bind
-  (("C-," . parinfer-toggle-mode))
-  :init
-  (progn
-    (setq parinfer-extensions
-     '(defaults       ; should be included.
-       pretty-parens  ; different paren styles for different modes.
-       evil           ; If you use Evil.
-       paredit        ; Introduce some paredit commands.
-       smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-       smart-yank))   ; Yank behavior depend on mode.
-    (add-hook 'clojure-mode-hook #'parinfer-mode)
-    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'scheme-mode-hook #'parinfer-mode)
-    (add-hook 'lisp-mode-hook #'parinfer-mode)))
 
+;;Parinfer
+(bind-key "C-," #'parinfer-toggle-mode evil-insert-state-map)
+(setq parinfer-extensions
+ '(defaults       ; should be included.
+   pretty-parens  ; different paren styles for different modes.
+   evil           ; If you use Evil.
+   paredit        ; Introduce some paredit commands.
+   smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+   smart-yank))   ; Yank behavior depend on mode.
+(add-hook 'clojure-mode-hook #'parinfer-mode)
+(add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+(add-hook 'common-lisp-mode-hook #'parinfer-mode)
+(add-hook 'scheme-mode-hook #'parinfer-mode)
+(add-hook 'lisp-mode-hook #'parinfer-mode)
+
+;;Eclim
 (setq-default
   eclim-eclipse-dirs '("/Applications/Eclipse\\ Java.app/Contents/Eclipse")
   eclimd-executable "/Applications/Eclipse Java.app/Contents/Eclipse/eclimd"
